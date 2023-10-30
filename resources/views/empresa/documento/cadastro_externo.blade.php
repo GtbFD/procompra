a1ce92742456aa6a3e375b00e49bd8d16007a576
Gutemberg Ferreira Dantas <gutemberg.cz@gmail.com>
2023-10-30 09:25:50 -0300

    Campos obrigatórios implementados no front-end

--- resources/views/empresa/documento/cadastro_externo.blade.php
+++ resources/views/empresa/documento/cadastro_externo.blade.php
@@ -8,26 +8,26 @@
         <form action="{{route('finish-creation')}}" method="post" enctype="multipart/form-data">
             @csrf
             <label for="empresa" class="form-label">CNPJ (Sem pontuação)</label>
-            <input class="form-control"  name="cnpj" placeholder="Ex.: 00111222333000455" type="text" id="empresa">
+            <input class="form-control" required  name="cnpj" placeholder="Ex.: 00111222333000455" type="text" id="empresa">
             <br>
 
             <label for="federal" class="form-label">CERTIDÃO NEGATIVA - FEDERAL</label>
-            <input class="form-control"  name="certidoes[federal]" type="file" id="federal"><br>
+            <input class="form-control" required  name="certidoes[federal]" type="file" id="federal"><br>
 
             <label for="estadual" class="form-label">CERTIDÃO NEGATIVA - ESTADUAL</label>
-            <input class="form-control"  name="certidoes[estadual]" type="file" id="estadual"><br>
+            <input class="form-control" required  name="certidoes[estadual]" type="file" id="estadual"><br>
 
             <label for="municipal" class="form-label">CERTIDÃO NEGATIVA - MUNICIPAL</label>
-            <input class="form-control"  name="certidoes[municipal]" type="file" id="municipal"><br>
+            <input class="form-control" required name="certidoes[municipal]" type="file" id="municipal"><br>
 
             <label for="falencia" class="form-label">CERTIDÃO NEGATIVA - FALÊNCIA/CONCORDATA</label>
-            <input class="form-control"  name="certidoes[falencia]" type="file" id="falencia"><br>
+            <input class="form-control" required name="certidoes[falencia]" type="file" id="falencia"><br>
 
             <label for="fgts" class="form-label">CERTIDÃO NEGATIVA - FGTS</label>
-            <input class="form-control"  name="certidoes[fgts]" type="file" id="fgts"><br>
+            <input class="form-control" required name="certidoes[fgts]" type="file" id="fgts"><br>
 
             <label for="trabalhista" class="form-label">CERTIDÃO NEGATIVA - TRABALHISTA</label>
-            <input class="form-control"  name="certidoes[trabalhista]" type="file" id="trabalhista"><br>
+            <input class="form-control" required name="certidoes[trabalhista]" type="file" id="trabalhista"><br>
 
             <button type="submit" class="btn btn-success ">Cadastrar</button>
         </form>
